cb04477f77de43d7805781d27981f11ef7af2146
zayed-reza <zayedreza500@gmail.com>
2021-09-30 18:22:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Rating } from '@mui/material';
+import Newspaper from './components/Newspaper/Newspaper';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained">My First Button</Button>
+      <Rating name="read-only" value={3} readOnly />
+      <Newspaper></Newspaper>
     </div>
   );
 }
---
+++ src/components/Newspaper/Newspaper.js
@@ -0,0 +1,26 @@
+import { Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import News from '../News/News';
+
+const Newspaper = () => {
+    const [articles, setArticles] = useState([]);
+    useEffect(() => {
+        fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-30&sortBy=publishedAt&apiKey=1aefcfb6ab63462cb936e608859ce4e0')
+            .then(res => res.json())
+            .then(data => setArticles(data.articles))
+    }, [])
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {
+                    articles.map(article => <Grid item xs={2} sm={4} md={4} >
+                        <News article={article}></News>
+                    </Grid>)
+                }
+            </Grid>
+        </Box>
+    );
+};
+
+export default Newspaper;
